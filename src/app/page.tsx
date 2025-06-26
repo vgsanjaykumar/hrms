fe380236271cfc71351085a1d2d1a54d97abb005
vgsanjaykumar <143058056+vgsanjaykumar@users.noreply.github.com>
2025-06-26 12:27:33 +0530

    profile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,21 +6,21 @@ export default function Home() {
   const router = useRouter();
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-[#f0fdfc]">
+    <div className="min-h-screen flex items-center justify-center bg-violet-100">
       <div className="text-center space-y-6">
-        <h1 className="text-4xl font-bold text-[#34b6b8]">Welcome to  HRMS</h1>
+        <h1 className="text-4xl font-bold text-violet-700">Welcome to  HRMS</h1>
         <p className="text-gray-600">Efficient HR management made easy.</p>
 
         <div className="flex justify-center gap-4">
           <button
             onClick={() => router.push('/login')}
-            className="bg-[#34b6b8] text-white px-6 py-2 rounded-full font-medium hover:bg-[#2aa8a9]"
+            className="bg-violet-500 text-white px-6 py-2 rounded-full font-medium hover:bg-violet-800"
           >
             Login
           </button>
           <button
             onClick={() => router.push('/signup')}
-            className="border border-[#34b6b8] text-[#34b6b8] px-6 py-2 rounded-full font-medium hover:bg-[#e6fcfb]"
+            className="border border-violet-500 text-violet-600 px-6 py-2 rounded-full font-medium hover:bg-violet-200"
           >
             Signup
           </button>
